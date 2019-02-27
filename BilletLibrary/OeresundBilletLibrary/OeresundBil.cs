32e4cf069d37e9f87497310b3c104b037e20379d
Henrik Andersen <henr563a@edu.easj.dk>
2019-02-27 09:22:22 +0100

    Opg 10

---
+++ BilletLibrary/OeresundBilletLibrary/OeresundBil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OeresundBilletLibrary
+{
+    using BilletLibrary;
+    public class OeresundBil : Bil
+    {
+        public OeresundBil(string nummerplade, DateTime dato, bool brobizz)
+            : base(nummerplade, dato, brobizz)
+        {
+        }
+
+        public override decimal Pris()
+        {
+            if (Brobizz)
+            {
+                return 161;
+            }
+            else
+            {
+                return 410;
+            }
+        }
+
+        public override string Type()
+        {
+            return "Øresund Bil";
+        }
+    }
+}
---
+++ BilletLibrary/OeresundBilletLibrary/OeresundMC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OeresundBilletLibrary
+{
+    using BilletLibrary;
+    class OeresundMC : MC
+    {
+        public OeresundMC(string nummerplade, DateTime dato, bool brobizz)
+            : base(nummerplade, dato, brobizz)
+        {
+        }
+
+        public override decimal Pris()
+        {
+            if (Brobizz)
+            {
+                return 73;
+            }
+            else
+            {
+                return 210;
+            }
+        }
+
+        public override string Type()
+        {
+            return "Øresund MC";
+        }
+    }
+}
